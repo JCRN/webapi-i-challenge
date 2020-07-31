33ea33d2734cc2d1e2470c71172e91a76eb19632
Jason Clemons <clemons.jason@gmail.com>
2020-07-30 18:45:06 -0700

    added /users endpoint

--- index.js
+++ index.js
@@ -1,25 +1,32 @@
- const express = require('express')
+const express = require("express");
+const database = require("./data/db");
 
- const server = express()
+const server = express();
 
- server.get('/', (req, res) => {
-    res.send('Hello World')
- })
+server.get("/", (req, res) => {
+  res.send("Hello World");
+});
 
- server.get('/hobbits', (req, res) => {
-     const hobbits = [
-         {
-             id: 1,
-             name: 'Samwise Gamgee'
-         },
-         {
-             id: 2,
-             name: 'Frodo Baggins'
-         }
-     ]
+server.get("/hobbits", (req, res) => {
+  const hobbits = [
+    {
+      id: 1,
+      name: "Samwise Gamgee",
+    },
+    {
+      id: 2,
+      name: "Frodo Baggins",
+    },
+  ];
 
-     res.status(200).json(hobbits)
- })
+  res.status(200).json(hobbits);
+});
 
- server.listen(8000, () => console.log("API Running on port 8000"))
+server.get("/users", (req, res) => {
+  database
+    .find()
+    .then((result) => res.status(200).json(result))
+    .catch((err) => res.status(500).send(err));
+});
 
+server.listen(8000, () => console.log("API Running on port 8000"));
